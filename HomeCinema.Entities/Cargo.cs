eb0106f02f28372b085e33c4d6f7f7d6df25b0ec
09123363503 <58479317+09123363503@users.noreply.github.com>
2020-07-30 17:53:14 +0430

    990509

--- HomeCinema.Entities/Cargo.cs
+++ HomeCinema.Entities/Cargo.cs
@@ -8,29 +8,32 @@ using System.Threading.Tasks;
 
 namespace HomeCinema.Entities
 {
-    public class Unit : IEntityBase
+    class Cargo : IEntityBase
     {
-        public Unit()
-        {
-            MainArticles = new List<MainArticle>();
-        }
         public int ID { get; set; }
         [Required]
-        public string Code { get; set; }
+        public int Date { get; set; }
         [Required]
-        public string Name { get; set; }
+        public int Number { get; set; }
+        public bool Input { get; set; }
+        public bool Output { get; set; }
+        public bool Prodution { get; set; }
+        [Required]
+        public int WHKeeper { get; set; }
+
+
+
         [DefaultValue(0)]
         public int RegisterID { get; set; }
         [DefaultValue(0)]
+        public Int64 RegisterDateTime { get; set; }
+        [DefaultValue(0)]
         public int EditID { get; set; }
-        [DefaultValue(0)]
-        public int DeleteID { get; set; }
-        [DefaultValue(0)]
-        public Int64 RegisterDatetime { get; set; }
         [DefaultValue(0)]
         public Int64 EditDateTime { get; set; }
         [DefaultValue(0)]
+        public int DeleteID { get; set; }
+        [DefaultValue(0)]
         public Int64 DeleteDateTime { get; set; }
-        public virtual ICollection<MainArticle> MainArticles { get; set; }
     }
 }
